52428b5d709eb9756bea5cde018f674817c00a7c
shaojintian <sjt@hnu.edu.cn>
2019-11-27 00:40:48 +0800

    1

--- src/a.ts
+++ src/a.ts
@@ -1,1 +1,12 @@
 let s : string = 'adwaw'
+
+
+function sub(x: number,y: number){
+    return x-y
+}
+
+function add(x: number,y: number){
+    return x+y
+}
+
+module.exports ={sub,add}
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,6 @@ let hello :string = 'hello'
 document.querySelectorAll('.app')[0].innerHTML = hello
 
 
-import m from 'moment'
-
-declare module 'moment'{
-    export function myF():void
-}
-
-m.myF()
 
 
 
